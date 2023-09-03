fa1c0393275957af0b176afbee56a1192f9d2a67
slw <offgridauthor@gmail.com>
2023-09-03 17:13:21 -0500

    add CRUD routes

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
+
 export const PORT = 5555
 
-export const mongoDBURL= 'mongodb+srv://<username>:<password>@book-store-mern.ox5hsrc.mongodb.net/?retryWrites=true&w=majority'
+export const mongoDBURL= 'mongodb+srv://root:root@book-store-mern.ox5hsrc.mongodb.net/?retryWrites=true&w=majority'
 
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { PORT, mongoDBURL } from './config.js'
 import mongoose from 'mongoose'
+import { Book } from './models/bookModel.js'
 
 const app = express()
 
@@ -10,7 +11,68 @@ app.get('/', (req, res) => {
     return res.status(234).send('Hello World!')
 })
 
+app.post('/books', async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.price || !req.body.publishYear) {
+            return res.status(400).send({ message: 'Missing required data' })
 
+        }
+        const book = {
+            title: req.body.title,
+            author: req.body.author,
+            price: req.body.price,
+            publishYear: req.body.publishYear,
+        }
+        const newBook = await Book.create(book)
+        return res.status(201).send({ message: 'New book created', data: newBook })
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send({ message: err.message })
+    }
+})
+
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find({})
+        return res.status(200).send({ data: books, count: books.length })
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send({ message: err.message })
+    }
+})
+
+app.put('/books/:id', async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.price || !req.body.publishYear) {
+            return res.status(400).send({ message: 'Missing required data' })
+        }
+        const { id } = req.params
+        const result = await Book.findByIdAndUpdate(id, req.body)
+        if(!result) {
+            return res.status(404).send({ message: 'Book not found' })
+        }
+        return res.status(200).send({ message: 'Book updated' })
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send({ message: err.message })
+    }
+})
+
+
+app.get('/books/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const book = await Book.findById(id)
+        return res.status(200).send({ data: book })
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send({ message: err.message })
+    }
+})
 
 
 
